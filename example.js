b7c487b78f5a3a4b95622376574e346291b6ec6d
ankur598 <ankursingh.as278@gmail.com>
2021-07-28 16:47:17 +0530

    wetherApp

--- example.js
+++ example.js
@@ -7,7 +7,7 @@
     })
     .catch(function (err) {
      console.log('err:', err);
-})*/
+})
 async function makeRequest() {
     try {
         let res = await fetch("https://reqres.in/api/users?page=2");
@@ -18,4 +18,15 @@ async function makeRequest() {
          console.log('err:', err);
     }
 }
-makeRequest()
+makeRequest()*/
+async function getWethers() {
+    let city = document.getElementById('city').value;
+    try { 
+    let wether = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d7462389c6e671076d5c5c0e5e252cf4`)
+    let data = await wether.json();
+    console.log(city, Math.round(data.main.temp-273))
+}
+catch (err) {
+    console.log('err:',err)
+}
+}
