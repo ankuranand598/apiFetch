54efc3ff2bbd67fe3dda1742fcba9b84cccd01bd
ankur598 <ankursingh.as278@gmail.com>
2021-07-28 15:55:57 +0530

    async

--- example.js
+++ example.js
@@ -1,8 +1,21 @@
-fetch('https://reqres.in/api/users?page=2')
+/*fetch('https://reqres.in/api/users?page=2')
 //how to handle the promise,
     .then(function (res) {
-        console.log(res.json());
+        res.json().then(function (data) {
+          console.log('data:', data);  
+        })
     })
     .catch(function (err) {
      console.log('err:', err);
-})
+})*/
+async function makeRequest() {
+    try {
+        let res = await fetch("https://reqres.in/api/users?page=2");
+        let data = await res.json()
+        console.log('data:', data);
+    }
+    catch (err) {
+         console.log('err:', err);
+    }
+}
+makeRequest()
